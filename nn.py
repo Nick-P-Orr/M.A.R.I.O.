e66b8dc81e6a0a3752785b5b072c66e3bce38915
unknown <nick.p.orr@gmail.com>
2019-01-29 18:09:10 -0500

    Added imports that will likely be needed

--- nn.py
+++ nn.py
@@ -1,1 +1,11 @@
 #File for neural network
+from random import randint
+import numpy as np
+import tflearn
+import math
+from tflearn.layers.core import input_data, fully_connected
+from tflearn.layers.estimator import regression
+from statistics import mean
+from collections import Counter
+
+print('Hello')

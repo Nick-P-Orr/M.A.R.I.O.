e046fd7540a17503bd7f59353298501b659c00cc
Brendan Sileo <brendansileo@gmail.com>
2019-02-25 18:56:19 -0500

    smart boi

--- nn_B.py
+++ nn_B.py
@@ -23,14 +23,14 @@ class MarioNN:
 
     def generate_model(self):
         model = []
-        for i in range(randint(1,5)): #between 1 and 5 connections
+        for i in range(10): #between 1 and 5 connections
             trigger = randint(0,1) #0 for always, 1 for block, (not implemented->) 2 for enemy
             if trigger == 0: #don't need to choose a location
-                command = self.commands[randint(0,3)] #choose which command
+                command = randint(0,3) #choose which command
                 model.append([trigger, command, -1])
             else: #must choose a location to connect it to
                 location = [randint(0,13), randint(0,15)]
-                command = self.commands[randint(0,3)]
+                command = randint(0,3)
                 model.append([trigger, command, location])
         return model
 
@@ -41,9 +41,15 @@ class MarioNN:
                 if connection[1] not in commands:
                     commands.append(connection[1])
             else:
-                if grid[connection[2][0]][connection[2][1]] == connection[0]:
-                    if connection[1] not in commands:
-                        commands.append(connection[1])
+                try:
+                    if grid[connection[2][0]][connection[2][1]] == connection[0]:
+                        if connection[1] not in commands:
+                            commands.append(connection[1])
+                except:
+                    print('shits broke')
+                    print(grid)
+                    print(connection)
+                    exit()
         return commands
 
     def checkEqual(self, l):
@@ -53,10 +59,8 @@ class MarioNN:
         return True
 
     def test_models(self):
-        self.models = [[[0,'right',-1],[0,'jump',-1]]]
         self.last_models = []
         for model in self.models:
-            print(model)
             self.comm.newLevel()
             response = self.comm.passInput(None)
             grid = response[1]
@@ -66,22 +70,115 @@ class MarioNN:
                 commands = self.compile_commands(grid, model)
                 response = self.comm.passInput(commands)
                 response[0] -= 120
+                done = response[2]
                 grid = response[1]
                 if response[0] >= 0:
                     prev_scores.append(response[0])
                     if len(prev_scores) > 30:
                         if self.checkEqual(prev_scores[-30:]):
-                            self.last_models.append((model, prev_scores[-1]))
+                            self.last_models.append([model, prev_scores[-1]])
                             done = True
                 else:
-                    self.last_models.append((model, prev_scores[-1]))
+                    self.last_models.append([model, prev_scores[-1]])
                     done = True
-                time.sleep(.03)
-            print(self.last_models[-1][1])
-            time.sleep(.25)
+
+
+    def model(self):
+        network = input_data(shape=[None, 10, 1], name='input')
+        network = fully_connected(network, 25, activation='relu')
+        network = fully_connected(network, 1, activation='linear')
+        network = regression(network, optimizer='adam', learning_rate=1e-2, loss='mean_square', name='target')
+        model = tflearn.DNN(network)
+        return model
+
+    def preprocess_data(self, data):
+        processed_data = []
+        for pop in data:
+            processed_pop = []
+            for connection in pop[0]:
+                new_value = str(connection[0])+str(connection[1])
+                if connection[2] == -1:
+                    new_value += ('.1717')
+                else:
+                    new_value += '.'
+                    for i in connection[2]:
+                        new_value += (str(i))
+                processed_pop.append(float(new_value))
+            processed_data.append([processed_pop, pop[1]])
+        return processed_data
+
+    def preprocess_data2(self, data): #digs into array less because new prediction data doe not have outputs
+        processed_data = []
+        for connection in data:
+            new_value = str(connection[0])+str(connection[1])
+            if connection[2] == -1:
+                new_value += ('.1717')
+            else:
+                new_value += '.'
+                for i in connection[2]:
+                    new_value += (str(i))
+            processed_data.append(float(new_value))
+        return processed_data
+
+    def train_model(self, training_data, model):
+        X = np.array([i[0] for i in training_data]).reshape(-1, 10, 1)
+        y = np.array([i[1] for i in training_data]).reshape(-1, 1)
+        model.fit(X,y, n_epoch = 3, shuffle = True, run_id = 'test', show_metric=True)
+        model.save('test')
+        return model
+
+    def predict_new_models(self, model):
+        unprocessed = []
+        new_models = []
+        for i in range(20):
+            m = self.generate_model()
+            unprocessed.append(m)
+            m = self.preprocess_data2(m)
+            m = np.array(m).reshape(-1,10,1)
+            new_models.append(m)
+        results = []
+        for i, m in enumerate(new_models):
+            results.append((unprocessed[i], model.predict(m)[0][0]))
+        return results
+
+    def getScore(self, val):
+        return val[1]
+
+    def chooseNew(self, predictions):
+        predictions.sort(key=self.getScore)
+        chosen = predictions[0:10]
+        chosen = [i[0] for i in chosen]
+        for i in chosen:
+            self.models.append(i)
+
 
 m = MarioNN()
 m.initial_population()
 m.test_models()
-print(m.last_models)
+sum=0
+for i in m.last_models:
+    sum+=i[1]
+print("Random Avg: "+str(sum/10))
+
+mod = m.model()
+for x in range(15):
+    print(len(m.last_models))
+    training_data = m.preprocess_data(m.last_models)
+    trained = m.train_model(training_data, mod)
+    predictions = m.predict_new_models(trained)
+    m.chooseNew(predictions)
+    m.test_models()
+
+
+sum=0
+for i in m.last_models:
+    sum+=i[1]
+print("Learned Avg: "+str(sum/10))
+m.last_models.sort(key=m.getScore)
+chosen = m.last_models[0:10]
+chosen = [i[0] for i in chosen]
+for i in chosen:
+    print(i)
+
+
 
